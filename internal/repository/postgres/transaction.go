a800a83ec49a7370bc0e597069aa7866bfdc801d
Sparsh Soni <sparsh.officialwork@gmail.com>
2026-03-30 01:15:56 +0530

    feat(repository): Adds transaction adapter

---
+++ internal/repository/postgres/transaction.go
@@ -0,0 +1,105 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	domainerrors "github.com/PacemakerX/ledger-core/internal/errors"
+	"github.com/PacemakerX/ledger-core/internal/models"
+	"github.com/PacemakerX/ledger-core/internal/repository"
+)
+
+type transactionRepository struct{
+	pool *pgxpool.Pool
+}
+
+func NewTransactionRepository(pool *pgxpool.Pool) repository.TransactionRepository{
+	return  &transactionRepository{pool: pool}
+}
+
+
+func (r *transactionRepository) Create(ctx context.Context, tx repository.Tx, transaction *models.Transaction) (*models.Transaction, error){
+	
+	pgxTx:=tx.(pgx.Tx)
+
+	query:=`INSERT INTO transactions( idempotency_key, type, status, initiated_by, metadata )
+	VALUES($1, $2, $3, $4, $5)
+	RETURNING id, idempotency_key, type, status, initiated_by, metadata, created_at,completed_at`
+
+	err:=pgxTx.QueryRow(ctx,query,
+		transaction.IdempotencyKey,
+		transaction.Type,
+		transaction.Status,
+		transaction.InitiatedBy,
+		transaction.Metadata).Scan(
+			&transaction.ID,
+			&transaction.IdempotencyKey,
+			&transaction.Type,
+			&transaction.Status,
+			&transaction.InitiatedBy,
+			&transaction.Metadata,
+			&transaction.CreatedAt,
+			&transaction.CompletedAt,
+		)
+
+	if(err!=nil){
+		return nil,fmt.Errorf("transactionRepository.Create: %w",domainerrors.ErrDatabase)
+	}
+	return transaction,nil
+}
+
+func (r *transactionRepository) UpdateStatus(ctx context.Context, tx repository.Tx, id uuid.UUID, status string) error{
+	
+	pgxTx:=tx.(pgx.Tx)
+
+	query:=`UPDATE transactions
+			SET status= $3,
+			completed_at= $2
+			WHERE id = $1`
+
+	_,err:=pgxTx.Exec(ctx,query,id,time.Now(),status)
+
+
+
+	if err != nil {
+		return fmt.Errorf("transactionRepository.UpdateStatus: %w", domainerrors.ErrDatabase)
+	}
+	return nil
+}
+
+func (r *transactionRepository) GetByID(ctx context.Context, id uuid.UUID) (*models.Transaction, error){
+	
+	query:=`SELECT id, idempotency_key, type, status, initiated_by, metadata, created_at, completed_at 
+			FROM transactions
+			WHERE id = $1`
+
+	var transaction models.Transaction
+
+	err:=r.pool.QueryRow(ctx,query,id).Scan(
+		&transaction.ID,
+		&transaction.IdempotencyKey,
+		&transaction.Type,
+		&transaction.Status,
+		&transaction.InitiatedBy,
+		&transaction.Metadata,
+		&transaction.CreatedAt,
+		&transaction.CompletedAt,
+	)
+
+	if(err!=nil){
+
+		if errors.Is(err,pgx.ErrNoRows){
+			return  nil,domainerrors.ErrNotFound
+		}
+		return nil, fmt.Errorf("transactionRepository.GetByID: %w", 
+		domainerrors.ErrDatabase)
+	}
+	
+	return &transaction,nil
+}
